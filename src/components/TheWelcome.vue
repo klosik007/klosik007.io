d7b1249ee8f8f7fdfc8cdff15685b23c4da6ef88
Przemysław Kłos <pklos1992@gmail.com>
2023-12-13 12:55:54 +0100

    Welcome page content

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -4,6 +4,8 @@
 
 <template>
   <div>
-    
+    <h1>Welcome on my page!</h1>
+    <p class="lead">Fell free to get to know about me and my projects</p>
+    <p class="lead">Just navigate on the left</p>
   </div>
 </template>
