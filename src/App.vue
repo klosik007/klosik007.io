6228acb9ded9086776a07a5e5fde8a605c255010
Przemysław Kłos <pklos1992@gmail.com>
2024-02-08 13:18:36 +0100

    about me section improvements

--- src/App.vue
+++ src/App.vue
@@ -48,20 +48,23 @@ import TheMenu from './components/TheMenu.vue'
 
         <section id="aboutMe">
           <div class="row bg-light">
-<!--            <div class="col-12">-->
-              <div id="aboutMePhotos" class="float-start"></div>
-<!--            </div>-->
-            <div class="col-7 mt-4">
-              <div class="lh-1">
-                <p>who am i</p>
-                <p>HELLO</p>
+            <div class="col-6 mt-5 d-flex justify-content-center">
+              <div id="aboutMePhotos"></div>
+            </div>
+            <div class="col-6 mt-5 d-flex align-items-center">
+              <div>
+                <div class="lh-1">
+                  <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;who am i</p>
+                  <h2 class="hello">HELLO&nbsp;&nbsp;</h2>
+                  <div id="dashes"></div>
+                  <!--daszki-->
+                </div>
+                <p>
+                  Software developer with 2 years of experience in developing, maintaining and testing software applications.
+                  Strong analytical background with over 3 years of data analysis and scripting. Being a quick learner and having problem solving attitude.
+                  Team player at its best. Independent and goals-oriented work ethics.
+                </p>
               </div>
-
-              <p>
-                Software developer with 2 years of experience in developing, maintaining and testing software applications.
-                Strong analytical background with over 3 years of data analysis and scripting. Being a quick learner and having problem solving attitude.
-                Team player at its best. Independent and goals-oriented work ethics.
-              </p>
             </div>
           </div>
         </section>
@@ -101,13 +104,19 @@ import TheMenu from './components/TheMenu.vue'
 
   #aboutMePhotos {
     background-image: url("/src/assets/images/fff.png"), linear-gradient(90deg, rgba(2,0,36,1) 100%, rgba(9,9,121,1) 100%, rgba(0,212,255,1) 100%);
-    background-size: 300px 300px;
+    background-size: 500px 500px;
     background-repeat: no-repeat;
-    background-position: left 50% top 40%, left 30% top 20%;
-    width: 400px;
-    height: 400px;
+    background-position: left 80% top 70%, left 30% top 20%;
+    width: 550px;
+    height: 550px;
     max-width: 100%;
     max-height: 100%;
+  }
+
+  .hello {
+    display: inline-block;
+    border-bottom: 12px solid yellow;
+    line-height: 0.2;
   }
 
 </style>
