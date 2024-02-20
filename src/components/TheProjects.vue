38b695d51143d89ebf778bd6ebe1fa30fd35ff82
Przemysław Kłos <pklos1992@gmail.com>
2024-02-20 13:48:00 +0100

    Align items in projects to center

--- src/components/TheProjects.vue
+++ src/components/TheProjects.vue
@@ -1,40 +1,43 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
 <template>
   <section id="in-progress">
-    <div class="row bg-secondary-subtle">
+    <div class="row bg-secondary-subtle d-flex justify-content-center">
       <h1 class="text-decoration-underline lh-base">Projects in progress</h1>
-      <div class="col-6">
-        <div id="volumeInTimeManager" class="row mb-3">
-          <h2>Volume In Time Manager</h2>
-          <p class="lead">Simple tool for Android sounds on/off scheduling.</p>
-          <div class="align-items-center mt-3">
-            <img
-              class="img-thumbnail float-lg-start float-md-start float-sm-start px-2 me-2"
-              src="../assets/images/Screenshot_20240115-151931.png"
-              alt=""
-              loading="lazy"
-            />
-            <img
-              class="img-thumbnail float-lg-start float-md-start float-sm-start px-2 me-2"
-              src="../assets/images/Screenshot_20240115-151938.png"
-              alt=""
-              loading="lazy"
-            />
+      <div class="row">
+        <div class="col-6">
+          <div id="volumeInTimeManager" class="row mb-3">
+            <h2>Volume In Time Manager</h2>
+            <p class="lead">Simple tool for Android sounds on/off scheduling.</p>
+            <div class="mt-3">
+              <img
+                  class="img-thumbnail float-lg-start float-md-start float-sm-start px-2 me-2"
+                  src="../assets/images/Screenshot_20240115-151931.png"
+                  alt=""
+                  loading="lazy"
+              />
+              <img
+                  class="img-thumbnail float-lg-start float-md-start float-sm-start px-2 me-2"
+                  src="../assets/images/Screenshot_20240115-151938.png"
+                  alt=""
+                  loading="lazy"
+              />
+            </div>
           </div>
         </div>
-      </div>
-      <div class="col-6">
-        <div id="codeScanner" class="row mb-3">
-          <h2>Code Scanner</h2>
-          <p class="lead">Scan product code to know more about it!</p>
-          <div>
-            <img
-              class="img-thumbnail float-lg-start float-md-start float-sm-start px-2 me-2"
-              src="../assets/images/Screenshot_20240115-154109.png"
-              alt=""
-              loading="lazy"
-            />
+        <div class="col-6">
+          <div id="codeScanner" class="row mb-3">
+            <h2>Code Scanner</h2>
+            <p class="lead">Scan product code to know more about it!</p>
+            <div class="mt-3">
+              <img
+                class="img-thumbnail float-lg-start float-md-start float-sm-start px-2 me-2"
+                src="../assets/images/Screenshot_20240115-154109.png"
+                alt=""
+                loading="lazy"
+              />
+            </div>
           </div>
         </div>
       </div>
