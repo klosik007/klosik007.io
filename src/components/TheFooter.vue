dc9d1eaf3bc1ecca8d1423726630485732665326
Przemysław Kłos <pklos1992@gmail.com>
2024-02-23 11:40:39 +0100

    Footer margins improvements

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -4,8 +4,10 @@
 
 <template>
   <div class="row bg-dark-subtle">
-    <p class="text-center">Copyright Przemysław Kłos @2024</p>
-    <p class=text-center>Background Photo by <a href="https://unsplash.com/@ilyapavlov?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Ilya Pavlov</a> on <a href="https://unsplash.com/photos/monitor-showing-java-programming-OqtafYT5kTw?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a></p>
+    <div class="mt-3">
+      <p class="text-center">Copyright Przemysław Kłos 2024</p>
+      <p class="text-center">Background Photo by <a href="https://unsplash.com/@ilyapavlov?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Ilya Pavlov</a> on <a href="https://unsplash.com/photos/monitor-showing-java-programming-OqtafYT5kTw?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a></p>
+    </div>
   </div>
 </template>
 
