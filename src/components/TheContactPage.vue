3edb819d8464d75dfefebece400c42aeb7aef1a8
Przemysław Kłos <pklos1992@gmail.com>
2024-02-23 11:16:24 +0100

    Contact section initial version

--- src/components/TheContactPage.vue
+++ src/components/TheContactPage.vue
@@ -1,13 +1,40 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div class="row bg-light">
-    <h1>This is an contact page</h1>
+  <div class="row bg-black">
+    <div class="text-center mt-3">
+      <h3 class="text-bg-warning inTouchTextInlineBlock">Let's get in touch!</h3>
+      <h2 class="text-white">CONTACT ME</h2>
+      <div class="row">
+        <div class="col-4">
+          <img src="../assets/icons/envelope.svg" alt="Bootstrap" width="32" height="32" />
+          <a href="mailto:pklos1992@gmail.com" target="_blank">pklos1992@gmail.com</a>
+        </div>
+        <div class="col-4">
+          <img src="../assets/icons/phone.svg" alt="Bootstrap" width="32" height="32" />
+          <a href="tel:+48512655100" target="_blank">+48512655100</a>
+        </div>
+        <div class="col-4">
+          <img src="../assets/icons/geo-alt.svg" alt="Bootstrap" width="32" height="32" />
+          <span class="text-white">Gdańsk</span>
+        </div>
+      </div>
+      <a href="https://github.com/klosik007" target="_blank"
+        ><img class="px-2" src="../assets/icons/github.svg" alt=""
+      /></a>
+      <a href="https://www.linkedin.com/in/pklos" target="_blank"
+        ><img class="px-2" src="../assets/icons/linkedin.svg" alt=""
+      /></a>
+    </div>
   </div>
 </template>
 
 <style scoped>
+img:hover {
+  filter: invert(50%);
+}
 
+.inTouchTextInlineBlock {
+  display: inline-block;
+}
 </style>
