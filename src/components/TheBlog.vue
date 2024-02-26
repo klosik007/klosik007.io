64e4f240bb14b3c953983f83b1227857c6e844c3
Przemysław Kłos <pklos1992@gmail.com>
2024-02-26 11:46:41 +0100

    Use of pinia for defining blogs posts visibility

--- src/components/TheBlog.vue
+++ src/components/TheBlog.vue
@@ -6,55 +6,60 @@
           <h1 class="fw-bold">{{ post.title }}</h1>
           <p>Created at: {{ getFormattedDate(post.created_at) }}</p>
           <p v-html="post.content"></p>
-          <hr>
+          <hr />
         </div>
       </div>
       <div class="col-2">
-        <div v-if="posts == null">
+        <div v-if="posts != null">
           <div v-for="(post, index) in posts" v-bind:key="index">
             <a :href="'/blog#' + post.title">{{ post.title }}</a>
           </div>
         </div>
-        <div v-else>
-          Brak postów
-        </div>
       </div>
     </div>
   </div>
-
 </template>
 
 <script lang="ts">
-    import axios from 'axios'
-    import { defineComponent } from 'vue'
-    import moment from 'moment'
-    import { postsDataUrl } from '/src/constants'
+import axios from 'axios'
+import { defineComponent } from 'vue'
+import moment from 'moment'
+import { postsDataUrl } from '/src/constants'
+import {useBlogSectionStore} from "@/stores/blogSection";
 
-    export default defineComponent({
-        data() {
-            return {
-                posts: null
-            }
-        },
+export default defineComponent({
+  data() {
+    return {
+      posts: null
+    }
+  },
 
-        async created() {
-            await this.getPosts()
-        },
+  setup() {
+    const store = useBlogSectionStore();
+    return { store };
+  },
 
-        methods: {
-            async getPosts() {
-                let postsLocal = await axios.get(postsDataUrl)
-                this.posts = postsLocal.data
-            },
+  async created() {
+    await this.getPosts()
+  },
 
-            getFormattedDate(date: string) {
-                return moment(date).format("YYYY-MM-DD")
-            }
-        }
-    })
+  methods: {
+    async getPosts() {
+      let postsLocal = await axios.get(postsDataUrl)
+      this.posts = postsLocal.data
 
+      if (this.posts !== null) {
+        this.store.setBlogSectionVisible();
+      } else {
+        this.store.setBlogSectionInvisible();
+      }
+    },
+
+    getFormattedDate(date: string) {
+      return moment(date).format('YYYY-MM-DD')
+    }
+  }
+})
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,23 +1,33 @@
 <script lang="ts">
-  import { defineComponent } from 'vue'
+import { defineComponent } from 'vue'
+import { useBlogSectionStore } from '@/stores/blogSection'
 
-  export default defineComponent({
-    data() {
-      return {
-        activeSections: ["active", "", "", "", ""]
+export default defineComponent({
+  data() {
+    return {
+      activeSections: ['active', '', '', '', ''],
+    }
+  },
+
+  setup() {
+    const store = useBlogSectionStore();
+    return { store };
+  },
+
+  methods: {
+    setSectionAsActive(idx: number) {
+      for (let i = 0; i < this.activeSections.length; i++) {
+        this.activeSections[i] = ''
       }
+
+      this.activeSections[idx] = 'active'
     },
 
-    methods: {
-      setSectionAsActive(idx: number) {
-        for (let i = 0; i < this.activeSections.length; i++) {
-          this.activeSections[i] = '';
-        }
-
-        this.activeSections[idx] = 'active';
-      }
+    blogSectionVisible() {
+      return this.store.blogSectionVisible
     }
-  });
+  }
+})
 </script>
 
 <template>
@@ -28,19 +38,49 @@
     <div class="collapse navbar-collapse d-flex justify-content-center col-8">
       <ul class="nav nav-underline lh-1">
         <li class="nav-item px-1">
-          <a @click="setSectionAsActive(0)" class="nav-link text-white" :class="[activeSections[0]]" href="#">Home</a>
+          <a
+            @click="setSectionAsActive(0)"
+            class="nav-link text-white"
+            :class="[activeSections[0]]"
+            href="#"
+            >Home</a
+          >
         </li>
         <li class="nav-item px-1">
-          <a @click="setSectionAsActive(1)" class="nav-link text-white" :class="[activeSections[1]]" href="#aboutMe">About Me</a>
+          <a
+            @click="setSectionAsActive(1)"
+            class="nav-link text-white"
+            :class="[activeSections[1]]"
+            href="#aboutMe"
+            >About Me</a
+          >
         </li>
         <li class="nav-item px-1">
-          <a @click="setSectionAsActive(2)" class="nav-link text-white" :class="[activeSections[2]]" href="#myProjects">My Projects</a>
+          <a
+            @click="setSectionAsActive(2)"
+            class="nav-link text-white"
+            :class="[activeSections[2]]"
+            href="#myProjects"
+            >My Projects</a
+          >
+        </li>
+        <li v-if="blogVisible" class="nav-item px-1">
+          <a
+            @click="setSectionAsActive(3)"
+            class="nav-link text-white"
+            :class="[activeSections[3]]"
+            href="#blog"
+            >Blog</a
+          >
         </li>
         <li class="nav-item px-1">
-          <a @click="setSectionAsActive(3)" class="nav-link text-white" :class="[activeSections[3]]" href="#blog">Blog</a>
-        </li>
-        <li class="nav-item px-1">
-          <a @click="setSectionAsActive(4)" class="nav-link text-white" :class="[activeSections[4]]" href="#contact">Contact</a>
+          <a
+            @click="setSectionAsActive(4)"
+            class="nav-link text-white"
+            :class="[activeSections[4]]"
+            href="#contact"
+            >Contact</a
+          >
         </li>
       </ul>
     </div>
@@ -51,5 +91,4 @@
 .nav-link.active {
   border-bottom: solid 0.25rem yellow;
 }
-
 </style>
